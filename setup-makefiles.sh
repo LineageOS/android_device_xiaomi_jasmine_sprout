7478ab28dd25fd23e5da4e97c3c4c3136712b752
Isaac Chen <isaacchen@isaacchen.cn>
2018-12-02 10:39:28 +0800

    wayne: Inherit from wayne-common
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -17,31 +17,9 @@
 
 set -e
 
-DEVICE=wayne
-VENDOR=xiaomi
+export DEVICE=wayne
+export VENDOR=xiaomi
+export INITIAL_COPYRIGHT_YEAR=2018
+export DEVICE_COMMON=wayne-common
 
-INITIAL_COPYRIGHT_YEAR=2018
-
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-LINEAGE_ROOT="$MY_DIR"/../../..
-
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
-
-# Copyright headers and guards
-write_headers
-
-write_makefiles "$MY_DIR"/proprietary-files.txt true
-
-# Finish
-write_footers
+./../../$VENDOR/$DEVICE_COMMON/setup-makefiles.sh $@
