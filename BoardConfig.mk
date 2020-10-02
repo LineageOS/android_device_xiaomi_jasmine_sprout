99c36940f81e08a0abe0288ef478821cdd8cc594
Michael Bestas <mkbestas@lineageos.org>
2020-10-03 00:08:44 +0300

    jasmine_sprout: Update SPL from QKQ1.190910.002/V11.0.14.0.QDIMIXM
    Change-Id: Idda751a309581fb42a4d3d0fe6e028cc9501cb40

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,7 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-08-05
+VENDOR_SECURITY_PATCH := 2020-09-05
 
 # Inherit the proprietary files
 include vendor/xiaomi/jasmine_sprout/BoardConfigVendor.mk
