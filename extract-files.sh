95545f742484371fa7abbc1ec16b8833b4615833
TH779 <i@779.moe>
2021-08-21 23:32:14 +0800

    Revert "jasmine_sprout: Commonize fingerprint blobs"
     * Also adapt to blob_fixup function.
    
    This reverts commit 911cc3f5acbf3f4fe050ffdf09c2228e3ab5e81c.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I3724ae12614084904d262a49d52007f520945934

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/libgf_ca.so)
+            sed -i 's|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g' "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
